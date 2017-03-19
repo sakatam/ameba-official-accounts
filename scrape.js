52a45a8f60f5145d075efba390574c4de28b8f05
Makoto Sakata <maco.sakata@gmail.com>
2017-03-19 23:07:30 +0900

    more accuracy

--- scrape.js
+++ scrape.js
@@ -4,23 +4,34 @@ const cheerio = require("cheerio")
 
 let jobs = []
 
-for(let i = 1; i <= 12; i++) {
-  let url = `http://official.ameba.jp/search/birthday/birthday_${i}.html`
+let parse = function(url, body) {
+  let $ = cheerio.load(body)
+
+  let data = $('ul.srchDetlLi dl dd.name.clr p a').map(function(i, elem) {
+    let href, id, text, name;
+    try {
+      href = $(elem).attr("href")
+      id = href.match(/^http:\/\/ameblo.jp\/([\w-]+)\/?/)[1]
+      text = $(elem).text()
+      name = text.split('\n')[0]
+      return {id, name}
+    } catch(e) {
+      console.error('failed to extract name', {url, href, text})
+    }
+  }).get()
+
+  return data
+}
+
+for(let i = 1; i <= 44; i++) {
+  let url = `http://official.ameba.jp/kana/kana${i}.html`
   jobs.push(function(cb) {
     request(url, function(err, res, body) {
       if(err || res.statusCode != 200) {
         return cb(`failed to scrape ${url}. err: ${err} code: ${res.statusCode}`)
       }
 
-      let $ = cheerio.load(body)
-
-      let data = $('ul.imgLi.clr p a').map(function(i, elem) {
-        let id = $(elem).attr("href").match(/^http:\/\/ameblo.jp\/([\w-]+)\/?/)[1]
-        let name = $(elem).attr("title")
-        return {id, name}
-      }).get()
-
-      cb(null, data)
+      cb(null, parse(url, body))
     })
   })
 }
